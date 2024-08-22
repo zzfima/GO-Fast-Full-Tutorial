fa1a3df771cd17d7a8aed224c1219bb727d493c6
zzfima <zzfima@gmail.com>
2024-08-22 18:01:55 +0300

    add cap len

--- 4_arrays/smd/main.go
+++ 4_arrays/smd/main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println()
+
+	//array - fixed length, same type, indexable, contiguous in memory
+	var intArr [3]int32
+	intArr[0] = 12
+	fmt.Println(intArr[0])
+	fmt.Println(intArr[0:3])
+
+	//addresses are contiguous in memory
+	fmt.Println(&intArr[0])
+	fmt.Println(&intArr[1])
+	fmt.Println(&intArr[2])
+
+	var intArr2 [3]int32 = [3]int32{4, 5, 6}
+	fmt.Println(intArr2[0:3])
+
+	intArr3 := [3]int32{14, 15, 16}
+	fmt.Println(intArr3[0:3])
+
+	intArr4 := []int32{14, 15, 16, 17, 18}
+	fmt.Println(intArr4[0:5])
+	fmt.Println(intArr4)
+
+	//slices
+	fmt.Printf("capacity before %v, len before %v\n", cap(intArr4), len(intArr4))
+	intArr4 = append(intArr4, 77)
+	fmt.Printf("capacity after %v, len after %v\n", cap(intArr4), len(intArr4))
+	fmt.Println(intArr4)
 }
