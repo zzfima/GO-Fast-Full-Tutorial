eaf5d4957d05dc4eef2fd6fa8dcf5df7a181d8ba
zzfima <zzfima@gmail.com>
2024-08-23 13:36:15 +0300

    close channel

--- 10_channels/main.go
+++ 10_channels/main.go
@@ -8,6 +8,29 @@ import (
 func main() {
 	//lockSituation()
 	noLockSituation()
+	noLockChat()
+}
+
+func noLockChat() {
+	var ch = make(chan int)
+	go startChat(ch)
+	for i := range ch {
+		fmt.Println(i)
+	}
+}
+
+func startChat(ch chan int) {
+	//if dont close channel - deadlock
+	defer close(ch)
+
+	for {
+		time.Sleep(300 * time.Millisecond)
+		var cs = time.Now().Second()
+		if cs%10 == 0 {
+			break
+		}
+		ch <- cs
+	}
 }
 
 func noLockSituation() {
