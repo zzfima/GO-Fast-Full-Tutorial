7dc2a973fa49a7b52b31d4e803d42f320afb8e7c
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-08-27 08:35:03 +0300

    add fan in using select

--- 14_concurrency/fanin.go
+++ 14_concurrency/fanin.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func fanInClient() {
-	c := fanIn(boring("Joe"), boring("Mike"))
+	c := fanInWithSwitch(boring("Joe"), boring("Mike"))
 	for i := 0; i < 10; i++ {
 		fmt.Println(<-c)
 	}
@@ -21,6 +21,24 @@ func fanIn(input1, input2 <-chan string) <-chan string {
 	go func() {
 		for {
 			c <- <-input2
+		}
+	}()
+
+	return c
+}
+
+//Multiplexer(fan in) pattern. Combine channels into one channel
+func fanInWithSwitch(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		for {
+			select {
+			case s := <-input1:
+				c <- s
+			case s := <-input2:
+				c <- s
+			}
 		}
 	}()
 
